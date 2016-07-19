125d4b1e6756354cfd19071fc7c61a4bbeaf18bb
Sébastien Fievet <_@sebastien-fievet.fr>
2016-07-19 11:23:37 +0200

    Add GPLv3 license header and RETURN documentation

--- cloud/digital_ocean/digital_ocean_tag.py
+++ cloud/digital_ocean/digital_ocean_tag.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 DOCUMENTATION = '''
 ---
 module: digital_ocean_tag
@@ -69,6 +83,25 @@ EXAMPLES = '''
   digital_ocean_tag:
     name: dbserver
     state: absent
+'''
+
+
+RETURN = '''
+data:
+    description: a DigitalOcean Tag resource
+    returned: success and no resource constraint
+    type: dict
+    sample: {
+        "tag": {
+        "name": "awesome",
+        "resources": {
+          "droplets": {
+            "count": 0,
+            "last_tagged": null
+          }
+        }
+      }
+    }
 '''
 
 import json
